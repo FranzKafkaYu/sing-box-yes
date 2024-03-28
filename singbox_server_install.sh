deeedc9cce7a6be8b6afcbd5320d965a7177d94b
BI4NBN <58468656+bi4nbn@users.noreply.github.com>
2024-03-28 15:44:01 +0800

    1.9.0-rc.3

--- singbox_server_install.sh
+++ singbox_server_install.sh
@@ -6,7 +6,7 @@ openssl ecparam -genkey -name prime256v1 -out /etc/hysteria/private.key
 openssl req -new -x509 -days 36500 -key /etc/hysteria/private.key -out /etc/hysteria/cert.pem -subj "/CN=bing.com"
 
 # 运行安装脚本
-bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/sing-box-yes/master/install.sh) install
+bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/sing-box-yes/master/install.sh) install 1.9.0-rc.3
 
 # 创建/修改配置文件
 wget -O /usr/local/etc/sing-box/config.json https://github.com/bi4nbn/sing-box_auto/raw/main/server.json
